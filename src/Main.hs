57ecd6d25c9aeaac8773e457d89ad0c9faafc62e
drula <adudinov@yandex.by>
2018-05-01 16:40:19 +0300

    Write result to files (draft)

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 import System.Environment (getArgs, getProgName)
 import Data.List (intersperse)
--- import System.FilePath (addExtension, dropExtension)
+import System.FilePath (addExtension, dropExtension)
 import Lex
 import Synt
 import CTypes
@@ -16,14 +16,19 @@ main = do
             contents <- readFile inputFileName
             let tree = synt $ alexScanTokens contents
             let (result @ (CContent typedefs functions)) = translate tree
+
+            writeFiles inputFileName result
+
             putStrLn $ "Typedefs:\n" ++ show typedefs ++ "\n"
             putStrLn $ "Functions:\n" ++ show functions ++ "\n"
             putStrLn $ "String representation:\n\n" ++ (concat $ intersperse "\n" $ textify result)
     where
-        {-writeFiles inputFileName header = do
+        writeFiles inputFileName (CContent typedefs functions) = do
             let baseFileName = (dropExtension inputFileName) ++ "_parser"
             let parserHeaderFileName = addExtension baseFileName "h"
-            writeFile parserHeaderFileName $ show header-}
+            let parserSourceFileName = addExtension baseFileName "c"
+            writeFile parserHeaderFileName $ concat $ intersperse "\n" $ concat $ map textify typedefs
+            writeFile parserSourceFileName $ concat $ intersperse "\n" $ concat $ map textify functions
 
         showUsage = do
             progName <- getProgName
